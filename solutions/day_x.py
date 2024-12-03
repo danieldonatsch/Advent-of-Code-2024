92df53764bfce94cb2f714e5ea62a1af17d9f9a2
Dani <18262962+danieldonatsch@users.noreply.github.com>
2024-12-03 16:07:48 +0100

    Update day_x.py: Can now handle verbose...

--- solutions/day_x.py
+++ solutions/day_x.py
@@ -2,9 +2,8 @@ from . import base_solution as bs
 
 
 class SolutionDayXX(bs.BaseSolution):
-    def __init__(self, day_num: int, example=False):
-        super().__init__(day_num, example)
-        print(f"{self.__class__.__name__} initialized")
+    def __init__(self, day_num: int, example=False, verbose=False):
+        super().__init__(day_num, example, verbose=verbose)
 
     def _star_1(self) -> None:
         """Fill in description and code...
