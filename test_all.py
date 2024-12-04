1d587ad1fc18ff61073e63555b19e6a23823aabb
Dani <18262962+danieldonatsch@users.noreply.github.com>
2024-12-04 19:51:07 +0100

    Create test_all.py

---
+++ test_all.py
@@ -0,0 +1,54 @@
+import time
+
+import solutions
+from run import get_advent_of_code_solution
+
+from pydoc import locate
+
+# Solution for samples.
+# Dict key is the day, first solution for star 1, second for star 2
+sample_solutions = {
+    1: (11, 31),
+    2: (2,4),
+    3: (161, 48),
+    4: (18, 9),
+
+}
+
+# Solution for samples.
+# Dict key is the day, first solution for star 1, second for star 2
+puzzle_solutions = {
+    1: (2742123, 21328497),
+    2: (479, 531),
+    3: (183788984, 62098619),
+    4: (2370, 1908)
+}
+
+
+def run_all_tests(example=True) -> None:
+    target_values = sample_solutions if example else puzzle_solutions
+
+    for day, values in sorted(target_values.items()):
+        solution = get_advent_of_code_solution(day=day, is_example=example)
+        val = values[0]
+        out = ''
+        if val == solution._star_1():
+            out += '*'
+        else:
+            out += 'x'
+        if len(values) == 1:
+            out += '-'
+        else:
+            val = values[1]
+            if val == solution._star_2():
+                out += '*'
+            else:
+                out += 'x'
+        print(f"{day: 2d}", out)
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    run_all_tests(example=True)
+
+    print("\nRunning all Advent of Code 2024 puzzles took", round(time.time() - t0, 4), "seconds")
